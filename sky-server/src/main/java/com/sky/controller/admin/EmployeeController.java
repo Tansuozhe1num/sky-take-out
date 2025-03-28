848954b490ef4219d211ca85c3a0799305456f30
Dream_startooo <14820205+dreamstartooo@user.noreply.gitee.com>
2025-03-28 10:36:48 +0800

    编辑员工信息代码开发

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -105,4 +105,24 @@ public class EmployeeController {
         employeeService.StartorStop(status, id);
         return Result.success();
     }
+
+    /***
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询员工信息")
+    public Result<Employee> getById(@PathVariable Long id) {
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    @ApiOperation("编辑员工信息")
+    public Result updateMessage(@RequestBody EmployeeDTO employeeDTO) {
+        log.info("编辑员工信息开始 {}", employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
+    }
 }
