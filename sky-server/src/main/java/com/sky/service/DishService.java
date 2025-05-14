9a5f31bf07ebd28ce3ca175da5b0eb00550f688b
dream_startooo <511017802@qq.com>
2025-05-14 10:39:04 +0800

    修复了部分小bug

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,7 +1,9 @@
 package com.sky.service;
 
+import com.github.pagehelper.Page;
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Select;
@@ -13,16 +15,12 @@ import java.util.List;
 
 
 public interface DishService {
-
-
     /**
      * 新增菜品和对应口味
      * @param dishDTO
      */
     public void saveWithFlavour(DishDTO dishDTO);
-
     PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
-
     /***
      * 菜品批量删除
      * @param ids
@@ -41,4 +39,5 @@ public interface DishService {
      * @param dishDTO
      */
     void updateWithFlavour(DishDTO dishDTO);
+
 }
