e08cf2813986935da85658448c889cfd27dc53c1
Dream_startooo <14820205+dreamstartooo@user.noreply.gitee.com>
2025-05-15 11:18:09 +0800

    wx小程序商品浏览业务代码

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -2,6 +2,7 @@ package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
@@ -66,5 +67,26 @@ public class DishController {
         log.info("修改菜品: {}", dishDTO);
         dishService.updateWithFlavour(dishDTO);
         return Result.success();
+    }
+
+    /***
+     * 启用禁用菜品状态
+     */
+    @PostMapping("status/{status}")
+    public Result StartOrstop(@PathVariable Integer status, Long id) {
+        dishService.StartorStop(status, id);
+        return Result.success();
+    }
+
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    @GetMapping("/list")
+    @ApiOperation("根据分类id查询菜品")
+    public Result<List<Dish>> list(Long categoryId){
+        List<Dish> list = dishService.list(categoryId);
+        return Result.success(list);
     }
 }
